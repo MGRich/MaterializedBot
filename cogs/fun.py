ae7368cb63ee9835494d3c8cb0375508baeae812
MGRich <29069561+MGRich@users.noreply.github.com>
2018-07-31 14:58:17 -0500

    Add files via upload

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,9 @@
-import discord, traceback
+import discord, traceback, time
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 import random
 from cogs.helprs.suggestions import suggestions
+from datetime import datetime
 
 class Fun:
     def __init__(self, bot):
@@ -28,6 +29,36 @@ class Fun:
         <message to say>"""
         await ctx.message.delete()
         await ctx.send(msg)
+
+    @commands.command()
+    async def quote(self, ctx, mid: int, memb: discord.Member=None):
+        try:
+            if memb:
+                try:
+                    msg = await memb.get_message(mid)
+                except:
+                    msg = await ctx.channel.get_message(mid)
+            else:
+                msg = await ctx.channel.get_message(mid)
+        except discord.NotFound:
+            return await ctx.send("Message not found. (this is pretty broken, so it's gonna appear a lot.)")
+        e = discord.Embed(description=msg.content, timestamp=msg.created_at)
+        if msg.edited_at:
+            tm = msg.edited_at#.replace(tzinfo=timezone.utc).astimezone(tz=None)
+            e.set_footer(text=f"Edited {tm.strftime('%D')} at {tm.strftime('%I:%M %p')} UTC")
+        e.set_author(name=msg.author.display_name, icon_url=msg.author.avatar_url_as(static_format='png'))
+        if len(msg.author.roles) > 0:
+            e.colour = msg.author.roles[-1].colour
+        await ctx.send(embed=e)
+
+    @commands.command(aliases=['fquote'])
+    async def fakequote(self, ctx, memb: discord.Member, *, cont):
+        e = discord.Embed(description=cont, timestamp=datetime.utcnow())
+        e.set_author(name=memb.display_name, icon_url=memb.avatar_url_as(static_format='png'))
+        if len(memb.roles) > 0:
+            e.colour = memb.roles[-1].colour
+        await ctx.send(embed=e)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
